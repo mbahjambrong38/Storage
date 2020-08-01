6b7824cc04fcc3c5c73fceab968a9270e971e02e
mbahjambrong38 <64255690+mbahjambrong38@users.noreply.github.com>
2020-08-01 19:40:36 +0700

    Add files via upload

---
+++ Read.php
@@ -0,0 +1,37 @@
+<?php
+    include 'Connection.php';
+
+    // SQL untuk menampilkan data
+    $sql = "SELECT * FROM Material";
+    
+    // masukkan ke method query
+    $query = $conn->query($sql);
+    
+    //siapkan variable penampung array
+    $result = array();
+
+    // fetch hasil eksekusi ke array menggunakan fetch_array / fetch_assoc (recomended fetch_assoc)
+    while($data = $query->fetch_assoc()){
+
+        //tampilkan
+        //print_r($data);
+        $result[] = $data;
+
+    }
+
+    // hitung banyak baris array
+    if(count($result) > 0){
+        $status = true;
+        $material = $result;
+    }else{
+        $status = false;
+        $material = null;
+    }
+
+    //convert array ke json (wajib)
+    $response_json = json_encode(['Status' => $status,
+                                'Material' => $material]);
+
+    echo $response_json;
+
+?>
